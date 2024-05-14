7cf00b6025bfbee3bd21a96dd698811f24d22d7f
LiuJianhua <liujhtr@inspur.com>
2024-05-14 11:32:43 +0800

    add arm64

--- build-linux.cmd
+++ build-linux.cmd
@@ -15,3 +15,18 @@ go build -ldflags -s -w -o helm-wrapper
 
 
 docker build -t registry.inspures.com/csp/heimdall/helm-wrapper:20240425 .
+
+# windows ---> arm64
+
+set CGO_ENABLED=0
+set GOOS=linux
+set GOARCH=arm64
+go build -ldflags -s -o /home/huary/code/helm-wrapper/bin/arm64/helm-wrapper
+
+## linux---> arm64
+GOOS=linux GOARCH=arm64 go build -ldflags -s -o /home/huary/code/helm-wrapper/bin/arm64/helm-wrapper
+
+## arm64 docker
+docker buildx version
+docker buildx create --use
+docker buildx build --platform=linux/arm64 -t registry.inspures.com/csp/heimdall/helm-wrapper:20240425-arm64 --push -f Dockerfile .
